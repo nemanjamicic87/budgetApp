b362723ef2c66edaa7e2dba8a3ed982b6616bc00
Nemanja Micic <nemanja.micic@novalite.rs>
2019-04-15 14:03:20 +0200

    Budget functionality added

--- app.js
+++ app.js
@@ -110,7 +110,11 @@ var UIController = (function(){
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     }
     
     return {
@@ -157,6 +161,19 @@ var UIController = (function(){
             fieldsArr[0].focus();
         },
 
+        displayBudget: function(obj){
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalIncome;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExpensee;
+           
+
+            if( obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+            }else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+            }
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
     }
@@ -194,7 +211,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
 
             // 3. Display the budget on the UI
-            console.log(budget);
+            UIController.displayBudget(budget);
     }
 
     var ctrlAddItem = function(){
@@ -223,6 +240,12 @@ var controller = (function(budgetCtrl, UICtrl) {
                    
     return {
         init: function() {
+            UIController.displayBudget({
+                budget: 0,
+                totalIncome: 0,
+                totalExpensee: 0,
+                percentage: -1
+            });
             setupEventListeners(); //We setup event listeners with this call
         }
     }
